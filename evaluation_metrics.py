8e5fa929f2aae8eaf52916d6bc637f4b747f45cd
Daniel Vargas Venegas <dvargas@bice.cl>
2024-02-10 18:23:18 -0300

    Adding prec-recall curve

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,7 +1,8 @@
 ''' Implementation of evaluation metrics for ML models - Recreating what scikit-learn does. '''
 
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve
+import matplotlib.pyplot as plt
 
 def accuracy(y_true: list, y_pred: list) -> float:
     '''
@@ -100,14 +101,44 @@ def recall(y_true: list, y_pred: list) -> int:
     precision = true_positives(y_true=y_true, y_pred=y_pred) / (true_positives(y_true=y_true, y_pred=y_pred) + false_negatives(y_true=y_true, y_pred=y_pred))
     return precision
 
+def prc(y_true: list, y_pred: list, display=False):
+    '''
+    Función que calcula el precision y recall curve.
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve un grafico con la curva de precision recall.
+    '''
+    x_recall = []
+    y_precision = []
+
+    thresholds = [0, 0.1, 0.15, 0.22, 0.29, 0.33, 0.45, 0.51, 0.55, 0.8 , 0.85, 0.9]
+    
+    for t in thresholds:
+        temp_pred = [x>=t for x in y_pred]
+        x_recall.append(recall(y_true=y_true, y_pred=temp_pred))
+        y_precision.append(precision(y_true=y_true, y_pred=temp_pred))
+    print(x_recall)
+    print(y_precision)
+    if display:
+        plt.plot(x_recall, y_precision)
+        plt.show()
+    
 
 if __name__ == '__main__':
 
-    y_true = [1, 1, 0, 0, 1, 0, 0, 0, 0]
-    y_pred = [1, 1, 1, 1, 0, 0, 1, 0, 0]
+    y_true = [1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    y_pred = [1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0]
+    y_pred_floats = [0.9, 0.8, 0.85, 0.55, 0, 0, 0.45, 0, 0, 0.33, 0, 0, 0.22, 0, 0.29, 0, 0.51, 0.10, 0, 0, 0.15]
+    #size = 1_500
+    #y_true = np.random.randint(2, size=size)
+    #y_pred = np.random.randint(2, size=size)
     print('Accuracy calculado a mano:', accuracy(y_true=y_true, y_pred=y_pred))
     print('Accuracy calculado con scikit learn:', accuracy_score(y_true=y_true, y_pred=y_pred))
     print('Precision calculada a mano:', precision(y_true=y_true, y_pred=y_pred))
     print('Precision calculado con scikit learn:', precision_score(y_true=y_true, y_pred=y_pred))
     print('Recall calculado a mano:', recall(y_true=y_true, y_pred=y_pred))
     print('Recall calculado con scikit learn:', recall_score(y_true=y_true, y_pred=y_pred))
+    prc(y_true=y_true, y_pred=y_pred_floats, display=True)
+    
