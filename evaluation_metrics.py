a8c169afa34f9a38b2fef1b13a37d16007ff8e86
Daniel Vargas Venegas <dvargas@bice.cl>
2024-02-10 18:42:24 -0300

    Se agregó prec recall en df y f1 score

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,8 +1,9 @@
 ''' Implementation of evaluation metrics for ML models - Recreating what scikit-learn does. '''
 
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve
+from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve, f1_score
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def accuracy(y_true: list, y_pred: list) -> float:
     '''
@@ -75,7 +76,7 @@ def false_negatives(y_true: list, y_pred: list) -> int:
 
     return np.array([x != y for x, y in zip(y_pred, y_true) if x == 0]).sum()
 
-def precision(y_true: list, y_pred: list) -> int:
+def precision(y_true: list, y_pred: list) -> float:
     '''
     Función que calcula el precision. Recordar que el precision es el porcentaje de veces que tu modelo dice que es un 1 y efectivamente es un 1.
     Args:
@@ -88,7 +89,7 @@ def precision(y_true: list, y_pred: list) -> int:
     precision = true_positives(y_true=y_true, y_pred=y_pred) / (true_positives(y_true=y_true, y_pred=y_pred) + false_positives(y_true=y_true, y_pred=y_pred))
     return precision
 
-def recall(y_true: list, y_pred: list) -> int:
+def recall(y_true: list, y_pred: list) -> float:
     '''
     Función que calcula el recall. Recordar que el recall es el porcentaje de aciertos de 1's sobre todo el universo de 1's. 
     Args:
@@ -98,12 +99,26 @@ def recall(y_true: list, y_pred: list) -> int:
         Devuelve la cantidad la precision.
     '''
     assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
-    precision = true_positives(y_true=y_true, y_pred=y_pred) / (true_positives(y_true=y_true, y_pred=y_pred) + false_negatives(y_true=y_true, y_pred=y_pred))
-    return precision
+    recall = true_positives(y_true=y_true, y_pred=y_pred) / (true_positives(y_true=y_true, y_pred=y_pred) + false_negatives(y_true=y_true, y_pred=y_pred))
+    return recall
+
+def f1(y_true: list, y_pred: list) -> float:
+    '''
+    Función que calcula el f1 score que es la armonic mean entre prec y recall.
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve el f1.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+    f1 = 2 * recall(y_true, y_pred) * precision(y_true, y_pred) / (recall(y_true, y_pred) + precision(y_true, y_pred))
+    return f1
 
 def prc(y_true: list, y_pred: list, display=False):
     '''
-    Función que calcula el precision y recall curve.
+    Función que calcula el precision y recall curve. Es decir, precision y recall para distintos cortes de probabilidad.
+    Los cortes de probabilidad son estáticos, pero no sé si se definen segun algo o algun criterio.
     Args:
         y_true: Contiene la lista de valores reales o ground truth.
         y_pred: Contiene los valores predichos por el modelo.
@@ -119,12 +134,38 @@ def prc(y_true: list, y_pred: list, display=False):
         temp_pred = [x>=t for x in y_pred]
         x_recall.append(recall(y_true=y_true, y_pred=temp_pred))
         y_precision.append(precision(y_true=y_true, y_pred=temp_pred))
-    print(x_recall)
-    print(y_precision)
+    
     if display:
         plt.plot(x_recall, y_precision)
         plt.show()
+
+def prc_df(y_true: list, y_pred: list):
+    '''
+    Función que calcula la precision y recall para distintos cortes.
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve un dataframe con precision y recall para distintos cortes de probabilidad
+    '''
+    x_recall = []
+    y_precision = []
+
+    thresholds = [0, 0.1, 0.15, 0.22, 0.29, 0.33, 0.45, 0.51, 0.55, 0.8 , 0.85, 0.9]
     
+    for t in thresholds:
+        temp_pred = [x>=t for x in y_pred]
+        x_recall.append(recall(y_true=y_true, y_pred=temp_pred))
+        y_precision.append(precision(y_true=y_true, y_pred=temp_pred))
+    
+    df = pd.DataFrame(data=
+                      {
+                          'thrs': thresholds,
+                          'recall': x_recall,
+                          'precision': y_precision
+                      }
+                      )
+    print(df.to_string())
 
 if __name__ == '__main__':
 
@@ -140,5 +181,9 @@ if __name__ == '__main__':
     print('Precision calculado con scikit learn:', precision_score(y_true=y_true, y_pred=y_pred))
     print('Recall calculado a mano:', recall(y_true=y_true, y_pred=y_pred))
     print('Recall calculado con scikit learn:', recall_score(y_true=y_true, y_pred=y_pred))
-    prc(y_true=y_true, y_pred=y_pred_floats, display=True)
+    #prc(y_true=y_true, y_pred=y_pred_floats, display=True)
+    #prc_df(y_true=y_true, y_pred=y_pred_floats)
+    print('F1 score calculado a mano:', f1(y_true, y_pred))
+    print('F1 score calculado con scikit learn:', f1_score(y_true, y_pred))
+
     
