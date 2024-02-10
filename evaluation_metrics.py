eceb2c8f90ca98261a7788a30b5ec97930dfd14a
Daniel Vargas Venegas <dvargas@bice.cl>
2024-02-10 13:29:51 -0300

    Precision implemented

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,7 +1,7 @@
 ''' Implementation of evaluation metrics for ML models - Recreating what scikit-learn does. '''
 
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score
 
 def accuracy(y_true: list, y_pred: list) -> float:
     '''
@@ -16,9 +16,84 @@ def accuracy(y_true: list, y_pred: list) -> float:
     assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
     return (np.array(y_true) == np.array(y_pred)).sum() / len(y_true)
 
+def true_positives(y_true: list, y_pred: list) -> int:
+    '''
+    Función para contar los true positives. Recordar que los TP son las veces en que tu modelo dice que es un 1 y efectivamente es un 1.
+
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve la cantidad de true positives.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+
+    return np.array([x == y for x, y in zip(y_pred, y_true) if x == 1]).sum()
+
+def true_negatives(y_true: list, y_pred: list) -> int:
+    '''
+    Función para contar los true negatives. Recordar que los TN son las veces en que tu modelo dice que es un 0 y efectivamente es un 0.
+
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve la cantidad de true negatives.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+
+    return np.array([x == y for x, y in zip(y_pred, y_true) if x == 0]).sum()
+
+
+def false_positives(y_true: list, y_pred: list) -> int:
+    '''
+    Función para contar los false positives. Recordar que los FP son las veces en que tu modelo dice que es un 1 y no es un 1, es un 0.
+
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve la cantidad de false positives.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+
+    return np.array([x != y for x, y in zip(y_pred, y_true) if x == 1]).sum()
+
+
+def false_negatives(y_true: list, y_pred: list) -> int:
+    '''
+    Función para contar los false negatives. Recordar que los FN son las veces es que tu modelo dice que es un 0 y es un 1.
+
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve la cantidad de false negatives.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+
+    return np.array([x != y for x, y in zip(y_pred, y_true) if x == 0]).sum()
+
+def precision(y_true: list, y_pred: list) -> int:
+    '''
+    Función que calcula el precision. Recordar que el precision es el porcentaje de veces que tu modelo dice que es un 1 y efectivamente es un 1.
+    Args:
+        y_true: Contiene la lista de valores reales o ground truth.
+        y_pred: Contiene los valores predichos por el modelo.
+    Returns:
+        Devuelve la cantidad la precision.
+    '''
+    assert len(y_true) == len(y_pred), "El largo de predicciones debe ser igual al largo ground truths"
+    precision = true_positives(y_true=y_true, y_pred=y_pred) / (true_positives(y_true=y_true, y_pred=y_pred) + false_positives(y_true=y_true, y_pred=y_pred))
+    return precision
+
+
 if __name__ == '__main__':
 
-    y_true = [1, 1, 0, 0, 0, 0, 0, 0, 0]
-    y_pred = [1, 1, 1, 1, 0, 0, 1, 0, 1]
+    y_true = [1, 1, 0, 0, 0, 0, 1, 0, 0]
+    y_pred = [1, 1, 1, 1, 0, 0, 1, 0, 0]
     print('Accuracy calculado a mano:', accuracy(y_true=y_true, y_pred=y_pred))
-    print('Accuracy calculado con scikit learn:', accuracy(y_true=y_true, y_pred=y_pred))
+    print('Accuracy calculado con scikit learn:', accuracy_score(y_true=y_true, y_pred=y_pred))
+    print('Precision calculada a mano:', precision(y_true=y_true, y_pred=y_pred))
+    print('Precision calculado con scikit learn:', precision_score(y_true=y_true, y_pred=y_pred))
+
